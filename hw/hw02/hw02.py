ebf1e57b43665236d1736baed22ccb3dfb8a228e
John DeNero <denero@berkeley.edu>
2024-09-11 12:36:28 -0700

    Wed Sep 11 12:36:28 PDT 2024

--- hw/hw02/hw02.py
+++ hw/hw02/hw02.py
@@ -1,103 +1,34 @@
-from operator import add, mul
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
 
-square = lambda x: x * x
-
-identity = lambda x: x
-
-triple = lambda x: 3 * x
-
-increment = lambda x: x + 1
-
-
-HW_SOURCE_FILE=__file__
-
-
-def product(n, term):
-    """Return the product of the first n terms in a sequence.
-
-    n: a positive integer
-    term:  a function that takes one argument to produce the term
-
-    >>> product(3, identity)  # 1 * 2 * 3
-    6
-    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
-    120
-    >>> product(3, square)    # 1^2 * 2^2 * 3^2
-    36
-    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
-    14400
-    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
-    24
-    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
-    162
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
     """
     "*** YOUR CODE HERE ***"
 
 
-def accumulate(fuse, start, n, term):
-    """Return the result of fusing together the first n terms in a sequence 
-    and start.  The terms to be fused are term(1), term(2), ..., term(n). 
-    The function fuse is a two-argument commutative & associative function.
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
 
-    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
-    15
-    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
-    26
-    >>> accumulate(add, 11, 0, identity) # 11 (fuse is never used)
-    11
-    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
-    25
-    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
-    72
-    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
-    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
-    19
-    """
-    "*** YOUR CODE HERE ***"
-
-
-def summation_using_accumulate(n, term):
-    """Returns the sum: term(1) + ... + term(n), using accumulate.
-
-    >>> summation_using_accumulate(5, square) # square(0) + square(1) + ... + square(4) + square(5)
-    55
-    >>> summation_using_accumulate(5, triple) # triple(0) + triple(1) + ... + triple(4) + triple(5)
-    45
-    >>> # This test checks that the body of the function is just a return statement.
-    >>> import inspect, ast
-    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
-    ['Expr', 'Return']
-    """
-    return ____
-
-
-def product_using_accumulate(n, term):
-    """Returns the product: term(1) * ... * term(n), using accumulate.
-
-    >>> product_using_accumulate(4, square) # square(1) * square(2) * square(3) * square()
-    576
-    >>> product_using_accumulate(6, triple) # triple(1) * triple(2) * ... * triple(5) * triple(6)
-    524880
-    >>> # This test checks that the body of the function is just a return statement.
-    >>> import inspect, ast
-    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
-    ['Expr', 'Return']
-    """
-    return ____
-
-
-def make_repeater(f, n):
-    """Returns the function that computes the nth application of f.
-
-    >>> add_three = make_repeater(increment, 3)
-    >>> add_three(5)
+    >>> a = hailstone(10)
+    10
+    5
+    16
     8
-    >>> make_repeater(triple, 5)(1) # 3 * (3 * (3 * (3 * (3 * 1))))
-    243
-    >>> make_repeater(square, 2)(5) # square(square(5))
-    625
-    >>> make_repeater(square, 3)(5) # square(square(square(5)))
-    390625
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
     """
     "*** YOUR CODE HERE ***"
 
