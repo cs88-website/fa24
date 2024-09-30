20a6a729ae56e3f13b595c9a9dadf783f4364887
John DeNero <denero@berkeley.edu>
2024-09-30 08:10:18 -0700

    Mon Sep 30 08:10:18 PDT 2024

--- lab/lab05/lab05.py
+++ lab/lab05/lab05.py
@@ -1,215 +1,64 @@
-HW_SOURCE_FILE=__file__
+def print_if(s, f):
+    """Print each element of s for which f returns a true value.
 
-
-def insert_items(s, before, after):
-    """Insert after into s after each occurrence of before and then return s.
-
-    >>> test_s = [1, 5, 8, 5, 2, 3]
-    >>> new_s = insert_items(test_s, 5, 7)
-    >>> new_s
-    [1, 5, 7, 8, 5, 7, 2, 3]
-    >>> test_s
-    [1, 5, 7, 8, 5, 7, 2, 3]
-    >>> new_s is test_s
-    True
-    >>> double_s = [1, 2, 1, 2, 3, 3]
-    >>> double_s = insert_items(double_s, 3, 4)
-    >>> double_s
-    [1, 2, 1, 2, 3, 4, 3, 4]
-    >>> large_s = [1, 4, 8]
-    >>> large_s2 = insert_items(large_s, 4, 4)
-    >>> large_s2
-    [1, 4, 4, 8]
-    >>> large_s3 = insert_items(large_s2, 4, 6)
-    >>> large_s3
-    [1, 4, 6, 4, 6, 8]
-    >>> large_s3 is large_s
-    True
-    """
-    "*** YOUR CODE HERE ***"
-
-
-def group_by(s, fn):
-    """Return a dictionary of lists that together contain the elements of s.
-    The key for each list is the value that fn returns when called on any of the
-    values of that list.
-
-    >>> group_by([12, 23, 14, 45], lambda p: p // 10)
-    {1: [12, 14], 2: [23], 4: [45]}
-    >>> group_by(range(-3, 4), lambda x: x * x)
-    {9: [-3, 3], 4: [-2, 2], 1: [-1, 1], 0: [0]}
-    """
-    grouped = {}
-    for ____ in ____:
-        key = ____
-        if key in grouped:
-            ____
-        else:
-            grouped[key] = ____
-    return grouped
-
-
-def count_occurrences(t, n, x):
-    """Return the number of times that x is equal to one of the
-    first n elements of iterator t.
-
-    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
-    >>> count_occurrences(s, 10, 9)
-    3
-    >>> t = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
-    >>> count_occurrences(t, 3, 10)
-    2
-    >>> u = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
-    >>> count_occurrences(u, 1, 3)  # Only iterate over 3
-    1
-    >>> count_occurrences(u, 3, 2)  # Only iterate over 2, 2, 2
-    3
-    >>> list(u)                     # Ensure that the iterator has advanced the right amount
-    [1, 2, 1, 4, 4, 5, 5, 5]
-    >>> v = iter([4, 1, 6, 6, 7, 7, 6, 6, 2, 2, 2, 5])
-    >>> count_occurrences(v, 6, 6)
-    2
-    """
-    "*** YOUR CODE HERE ***"
-
-
-def repeated(t, k):
-    """Return the first value in iterator t that appears k times in a row,
-    calling next on t as few times as possible.
-
-    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
-    >>> repeated(s, 2)
-    9
-    >>> t = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
-    >>> repeated(t, 3)
-    8
-    >>> u = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
-    >>> repeated(u, 3)
-    2
-    >>> repeated(u, 3)
+    >>> print_if([3, 4, 5, 6], lambda x: x > 4)
     5
-    >>> v = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
-    >>> repeated(v, 3)
-    2
+    6
+    >>> result = print_if([3, 4, 5, 6], lambda x: x % 2 == 0)
+    4
+    6
+    >>> print(result)  # print_if should return None
+    None
     """
-    assert k > 1
-    "*** YOUR CODE HERE ***"
+    for x in s:
+        "*** YOUR CODE HERE ***"
 
 
-def sprout_leaves(t, leaves):
-    """Sprout new leaves containing the labels in leaves at each leaf of
-    the original tree t and return the resulting tree.
+def close(s, k):
+    """Return how many elements of s that are within k of their index.
 
-    >>> t1 = tree(1, [tree(2), tree(3)])
-    >>> print_tree(t1)
+    >>> t = [6, 2, 4, 3, 5]
+    >>> close(t, 0)  # Only 3 is equal to its index
     1
-      2
-      3
-    >>> new1 = sprout_leaves(t1, [4, 5])
-    >>> print_tree(new1)
-    1
-      2
-        4
-        5
-      3
-        4
-        5
-
-    >>> t2 = tree(1, [tree(2, [tree(3)])])
-    >>> print_tree(t2)
-    1
-      2
-        3
-    >>> new2 = sprout_leaves(t2, [6, 1, 2])
-    >>> print_tree(new2)
-    1
-      2
-        3
-          6
-          1
-          2
+    >>> close(t, 1)  # 2, 3, and 5 are within 1 of their index
+    3
+    >>> close(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
+    4
+    >>> close(list(range(10)), 0)
+    10
     """
-    "*** YOUR CODE HERE ***"
+    count = 0
+    for i in range(len(s)):  # Use a range to loop over indices
+        "*** YOUR CODE HERE ***"
+    return count
 
 
-def partial_reverse(s, start):
-    """Reverse part of a list in-place, starting with start up to the end of
-    the list.
+def close_list(s, k):
+    """Return a list of the elements of s that are within k of their index.
 
-    >>> a = [1, 2, 3, 4, 5, 6, 7]
-    >>> partial_reverse(a, 2)
-    >>> a
-    [1, 2, 7, 6, 5, 4, 3]
-    >>> partial_reverse(a, 5)
-    >>> a
-    [1, 2, 7, 6, 5, 3, 4]
+    >>> t = [6, 2, 4, 3, 5]
+    >>> close_list(t, 0)  # Only 3 is equal to its index
+    [3]
+    >>> close_list(t, 1)  # 2, 3, and 5 are within 1 of their index
+    [2, 3, 5]
+    >>> close_list(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
+    [2, 4, 3, 5]
     """
-    "*** YOUR CODE HERE ***"
+    return [___ for i in range(len(s)) if ___]
 
 
+from math import sqrt
 
-# Tree Data Abstraction
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
 
-def tree(label, branches=[]):
-    """Construct a tree with the given label value and a list of branches."""
-    for branch in branches:
-        assert is_tree(branch), 'branches must be trees'
-    return [label] + list(branches)
-
-def label(tree):
-    """Return the label value of a tree."""
-    return tree[0]
-
-def branches(tree):
-    """Return the list of branches of the given tree."""
-    return tree[1:]
-
-def is_tree(tree):
-    """Returns True if the given tree is a tree, and False otherwise."""
-    if type(tree) != list or len(tree) < 1:
-        return False
-    for branch in branches(tree):
-        if not is_tree(branch):
-            return False
-    return True
-
-def is_leaf(tree):
-    """Returns True if the given tree's list of branches is empty, and False
-    otherwise.
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
     """
-    return not branches(tree)
-
-def print_tree(t, indent=0):
-    """Print a representation of this tree in which each node is
-    indented by two spaces times its depth from the root.
-
-    >>> print_tree(tree(1))
-    1
-    >>> print_tree(tree(1, [tree(2)]))
-    1
-      2
-    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> print_tree(numbers)
-    1
-      2
-      3
-        4
-        5
-      6
-        7
-    """
-    print('  ' * indent + str(label(t)))
-    for b in branches(t):
-        print_tree(b, indent + 1)
-
-def copy_tree(t):
-    """Returns a copy of t. Only for testing purposes.
-
-    >>> t = tree(5)
-    >>> copy = copy_tree(t)
-    >>> t = tree(6)
-    >>> print_tree(copy)
-    5
-    """
-    return tree(label(t), [copy_tree(b) for b in branches(t)])
+    return [___ for n in s if ___]
 
