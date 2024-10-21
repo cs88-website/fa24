c7d3be33dbdd6d6b03ba39aba8ccd94ebd400cf1
John DeNero <denero@berkeley.edu>
2024-10-21 14:12:17 -0700

    Mon Oct 21 14:12:17 PDT 2024

---
+++ assets/slides/15.py
@@ -0,0 +1,64 @@
+class Link:
+    """A linked list.
+
+    >>> s = Link(3, Link(4, Link(5)))
+    >>> s
+    Link(3, Link(4, Link(5)))
+    >>> print(s)
+    <3 4 5>
+    >>> s.first
+    3
+    >>> s.rest
+    Link(4, Link(5))
+    >>> s.rest.first
+    4
+    >>> s.rest.first = 7
+    >>> s
+    Link(3, Link(7, Link(5)))
+    >>> s.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s
+    Link(6, Link(7))
+    >>> print(s)
+    <6 7>
+    >>> print(s.rest)
+    <7>
+    >>> t = Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> t
+    Link(1, Link(Link(2, Link(3)), Link(4)))
+    >>> print(t)
+    <1 <2 3> 4>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+    #Not in scope!
+    def __repr__(self):
+        if self.rest:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+    #Not in scope!
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+def map_link(f, s):
+    """Return a linked list of f(x) for each x in s.
+
+    >>> evens = Link(4, Link(2, Link(6)))
+    >>> map_link(lambda x: x + 1, evens)
+    Link(5, Link(3, Link(7)))
+    >>> evens
+    Link(4, Link(2, Link(6)))
+    """
+    if s is Link.empty:
+        return s
+    return Link(f(s.first), map_link(f, s.rest))
